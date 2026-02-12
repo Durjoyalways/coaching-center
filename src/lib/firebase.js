0c95ebfe320838bc9b967c0371ad651a6870672e
Durjoyalways <td16122019@gmail.com>
2026-02-12 21:56:43 +0600

    added some change

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,16 +2,17 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// সরাসরি ভ্যালু না লিখে process.env ব্যবহার করা হয়েছে
 const firebaseConfig = {
-  apiKey: "AIzaSyCmKDegiqZ9XMlZ2w6pKmAPih0f9G4MZB0",
-  authDomain: "shikhar-alo-ec624.firebaseapp.com",
-  projectId: "shikhar-alo-ec624",
-  storageBucket: "shikhar-alo-ec624.firebasestorage.app",
-  messagingSenderId: "553481992909",
-  appId: "1:553481992909:web:7dc21c247931301c8d8784"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-// Next.js এ সার্ভার সাইড রেন্ডারিং এর সময় বারবার ইনিশিয়ালাইজেশন এড়াতে এই চেকটি জরুরি
+// Next.js এ সার্ভার সাইড রেন্ডারিং এর সময় বারবার ইনিশিয়ালাইজেশন এড়াতে এই চেকটি জরুরি
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
